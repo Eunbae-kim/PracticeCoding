27520e814683706f69a18b84962cb2089134acc9
DESKTOP-KA38J9L\rladm <rladmsqo98@gmail.com>
2021-02-12 00:44:10 +0900

    Convert to Fahrenheit

---
+++ python/2021_02_12.py
@@ -0,0 +1,9 @@
+# python
+# Converts Celsius to Fahrenheit
+# the formula :  Fahrenheit = (Celsius * 1.8) +32
+
+def convertToFahrenheith(celcius):
+    return (celsius * 1.8) + 32
+
+celsius = 200
+print("Celsius", celsius, "is ", convertToFahrenheith(celsius), " in Fahrenheit!")
