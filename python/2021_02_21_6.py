ba484ce06127339a1c1b1262fb3478ad903f82e9
DESKTOP-KA38J9L\rladm <rladmsqo98@gmail.com>
2021-02-13 00:48:30 +0900

    # Returns a lsit with n elements removed from the left

---
+++ python/2021_02_21_6.py
@@ -0,0 +1,10 @@
+# python
+# Returns a lsit with n elements removed from the left
+def removeElement(list, n):
+    print(list[n:])
+
+
+a = [1,2,3,4,5,6]
+removeElement(a,4)
+removeElement(a,2)
+removeElement(a,100)
