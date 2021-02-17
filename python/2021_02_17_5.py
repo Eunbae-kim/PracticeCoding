f4deeef71245edd38b5062fd2e49313dfc45b5da
DESKTOP-KA38J9L\rladm <rladmsqo98@gmail.com>
2021-02-17 23:58:38 +0900

    # REturns the reverse of a string

---
+++ python/2021_02_17_5.py
@@ -0,0 +1,10 @@
+# python
+# REturns the reverse of a string
+
+def reverseString(string):
+    return string[::-1]
+# we set nothing in start, end -> This means that we can get whole string
+# if we put step = -1 than we can get the reverse of a string
+
+
+print(reverseString("grace"))
